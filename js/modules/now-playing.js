2cc4933d4a512cfec6da537ac3879aa0b39acc17
HenryFabianBoadaRubio <156461496+HenryFabianBoadaRubio@users.noreply.github.com>
2024-05-13 10:11:34 -0500

    feat: :sparkles: Se configuran los modulos de consulta.
    Se realizan unas consultas en los modulos que posteriormente se usaran para la parte del front de la web.

---
+++ js/modules/now-playing.js
@@ -0,0 +1,25 @@
+
+export const allGetTrack  =async (query)=> {
+
+    const url = `https://spotify23.p.rapidapi.com/search/?q=${query}&type=tracks&offset=0&limit=1`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '6c4f920a2bmsh9df10d15ac2f0dep1fc722jsn6528d3dd99c2',
+            'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+        }
+    };
+    
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        let cancion=result.tracks.items[0].data.uri;
+        return cancion;
+
+
+
+
+    } catch (error) {
+        console.error(error);
+    }
+}
---
+++ js/modules/track.list.js
@@ -0,0 +1,29 @@
+
+
+export const getAllListTracks = async (query) => {
+
+
+
+    const url = `https://spotify23.p.rapidapi.com/search/?q=${query}&type=tracks&offset=0&limit=7`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '6c4f920a2bmsh9df10d15ac2f0dep1fc722jsn6528d3dd99c2',
+            'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+        }
+    };
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json()
+        let lista=[];
+        
+        for(let cancion of result.tracks.items) {
+            lista.push(cancion.data.uri)
+        } 
+        return lista;
+    
+    } catch (error) {
+        console.error(error);
+    }
+}
+
